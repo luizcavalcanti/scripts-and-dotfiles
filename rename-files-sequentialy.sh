9035d6818d5dd9b7451d10beab848af835f9a2b7
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2014-12-18 13:22:45 -0400

    Added directory parameter to rename-files-sequentialy
    Added directory parameter, so the scripts now goes into
    the directory (cd $DIR) before execute the procedure.
    
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- rename-files-sequentialy.sh
+++ rename-files-sequentialy.sh
@@ -1,10 +1,12 @@
 #command parameters
-EXTENSION=$1
+DIRECTORY=$1
+EXTENSION=$2
 
 a=1
+cd $DIRECTORY
 for i in *.$EXTENSION; do
     new=$(printf "%05d.$EXTENSION" ${a})
     echo ${i}
-    mv ${i} ${new}
+    mv "${i}" ${new}
     let a=a+1
 done
