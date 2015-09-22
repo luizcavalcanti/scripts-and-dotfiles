f2589d7edee90874ef9582b822949de967b55862
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2015-09-21 20:01:36 -0400

    solves small issues with rename-files-sequentialy.sh
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- rename-files-sequentialy.sh
+++ rename-files-sequentialy.sh
@@ -3,10 +3,10 @@ DIRECTORY=$1
 EXTENSION=$2
 
 a=1
-cd $DIRECTORY
+cd "$DIRECTORY" || exit
 for i in *.$EXTENSION; do
     new=$(printf "%05d.$EXTENSION" ${a})
-    echo ${i}
-    mv "${i}" ${new}
+    echo "${i}"
+    mv "${i}" "${new}"
     let a=a+1
 done
