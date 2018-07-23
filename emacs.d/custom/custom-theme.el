5eb5bc2b02613df84bb479eb42b8f4ee269ce8b1
Luiz Cavalcanti <luiz.cavalcanti@locaweb.com.br>
2018-07-23 10:01:42 -0300

    [emacs] reduce font size in linux
    Signed-off-by: Luiz Cavalcanti <luiz.cavalcanti@locaweb.com.br>

--- emacs.d/custom/custom-theme.el
+++ emacs.d/custom/custom-theme.el
@@ -33,7 +33,7 @@
   (set-face-attribute 'mode-line nil :font "Monaco"))
 
 (when (my-system-type-is-gnu)
-  (set-default-font "Monospace 12")
+  (set-default-font "Monospace 11")
   (set-face-attribute 'mode-line nil :font "Monospace"))
 
 ;; load spaceline/poweline
